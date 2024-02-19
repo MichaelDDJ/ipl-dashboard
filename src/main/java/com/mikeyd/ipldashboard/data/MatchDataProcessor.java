d3b16115ee0131021e08c62c0a4366c57b692990
MichaelDDJ <jrmdd1496@gmail.com>
2024-02-19 14:01:08 -0600

    Added all the processing for MatchDataProcessor. It creates new match instances line by line from match data.

--- src/main/java/com/mikeyd/ipldashboard/data/MatchDataProcessor.java
+++ src/main/java/com/mikeyd/ipldashboard/data/MatchDataProcessor.java
@@ -1,25 +1,59 @@
 package com.mikeyd.ipldashboard.data;
 
+import com.mikeyd.ipldashboard.model.Match;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import org.springframework.batch.item.ItemProcessor;
 
-    public class MatchDataProcessor implements ItemProcessor<MatchInput, Person> {
+import java.time.LocalDate;
 
-        private static final Logger log = LoggerFactory.getLogger(PersonItemProcessor.class);
+
+//Data Processor from Spring batch to take data from csv file and create Match instances
+//It takes MatchInput and creates Match
+public class MatchDataProcessor implements ItemProcessor<MatchInput, Match> {
+
+        private static final Logger log = LoggerFactory.getLogger(MatchDataProcessor.class);
 
         @Override
-        public Person process(final Person person) {
-            final String firstName = person.firstName().toUpperCase();
-            final String lastName = person.lastName().toUpperCase();
+        public Match process(final MatchInput matchInput) throws Exception{
 
-            final Person transformedPerson = new Person(firstName, lastName);
+            //new match instance
+            Match match = new Match();
 
-            log.info("Converting (" + person + ") into (" + transformedPerson + ")");
+            //id created as String so parse into long for Match model
+            match.setId(Long.parseLong(matchInput.getId()));
+            match.setCity(matchInput.getCity());
+            match.setDate(LocalDate.parse(matchInput.getDate()));
+            match.setPlayerOfMatch(matchInput.getPlayer_of_match());
+            match.setVenue(matchInput.getVenue());
 
-            return transformedPerson;
+            //Set team 1 and 2 depending on innings order
+            String firstInningsTeam, secondInningsTeam;
+
+            if("bat".equals(matchInput.getToss_decision())) {
+                firstInningsTeam = matchInput.getToss_winner();
+                secondInningsTeam = matchInput.getToss_winner().equals(matchInput.getTeam1())
+                        ? matchInput.getTeam2() : matchInput.getTeam1();
+            } else {
+                secondInningsTeam = matchInput.getToss_winner();
+                firstInningsTeam = matchInput.getToss_winner().equals(matchInput.getTeam1())
+                        ? matchInput.getTeam2() : matchInput.getTeam1();
+            }
+
+            match.setTeam1(firstInningsTeam);
+            match.setTeam2(secondInningsTeam);
+
+            match.setTossWinner(matchInput.getToss_winner());
+            match.setTossDecision(matchInput.getToss_decision());
+
+            match.setResult(matchInput.getResult());
+            match.setResultMargin(matchInput.getResult_margin());
+            match.setUmpire1(matchInput.getUmpire1());
+            match.setUmpire2(matchInput.getUmpire2());
+
+            return match;
         }
 
-    }
 }
+
